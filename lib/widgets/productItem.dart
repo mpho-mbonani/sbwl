f4f14e3f415f36bc8502eec50f74aefa25050ccd
Mpho Mbonani <43796404+mpho-mbonani@users.noreply.github.com>
2020-12-08 23:16:08 +0200

    extension change

--- lib/widgets/productItem.dart
+++ lib/widgets/productItem.dart
@@ -27,7 +27,7 @@ class ProductItem extends StatelessWidget {
           child: Hero(
             tag: product.id,
             child: FadeInImage(
-              placeholder: AssetImage('assets/images/product-placeholder.gif '),
+              placeholder: AssetImage('assets/images/product-placeholder.png '),
               image: NetworkImage(product.imageUrl),
               fit: BoxFit.cover,
             ),
